84634a6173c48d53611f9049ec10000aa5cc886e
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2026-03-14 21:25:36 +0530

    added env

--- src/api/text.js
+++ src/api/text.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 
-const url = "http://localhost:8080/"
+// const url = "http://localhost:8080/"
+const url = import.meta.env.ClipCloud_Backend_URL
 
 export const handleCheckPassword =async (password)=>{
     
